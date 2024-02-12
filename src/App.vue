f80a149ebc0afa03d05fd44c6931802cd050a9c4
Naresh Tak <taknaresh515@gmail.com>
2024-02-12 17:47:56 +0530

    change the a tag

--- src/App.vue
+++ src/App.vue
@@ -88,23 +88,24 @@ const showTopContainer = computed(() => {
       <div :class="[mobileClass]" v-if="showTopContainer"
         class="top_container relative lg:mt-[190px] mt-[230px] w-full mx-auto text-center bg-[#111111] p-6 lg:rounded-[20px] h-[500px] lg:h-[550px]">
         <div class="flex flex-col items-center">
-          <img src="./assets/1707458134767.jpg" class="h-[240px] w-[240px] absolute rounded-[20px] top-0 object-cover mt-[-200px]">
+          <img src="./assets/1707458134767.jpg"
+            class="h-[240px] w-[240px] absolute rounded-[20px] top-0 object-cover mt-[-200px]">
           <h2 class="mt-6 mb-2 text-3xl font-semibold text-white">Naresh Tak</h2>
           <span class="mb-4 text-[#A6A6A6] inline-block px-5 py-1.5 rounded-lg bg-[#1D1D1D] ">Ui/Ux Designer</span>
         </div>
         <div class="flex socail_icon justify-center">
           <button class="w-[2.5rem] h-[2.5rem] rounded-lg bg-[#212425] ml-[15px]"><a
-              href="https://www.instagram.com/lunatic_naresh1?igsh=cGVwZmIxdjU5bjJv" class="text-[#1773EA]"><i
-                class="fa-brands fa-instagram"></i></a></button>
+              href="https://www.instagram.com/lunatic_naresh1?igsh=cGVwZmIxdjU5bjJv" target="_blank"
+              class="text-[#1773EA]"><i class="fa-brands fa-instagram"></i></a></button>
           <button class="w-[2.5rem] h-[2.5rem] rounded-lg bg-[#212425] ml-[15px]"><a
-              href="https://x.com/Lunatic_Naresh?t=fCjBUwjalqrTpLxgy2BTYA&s=09" class="text-[#1C9CEA]"><i
+              href="https://x.com/Lunatic_Naresh?t=fCjBUwjalqrTpLxgy2BTYA&s=09" target="_blank" class="text-[#1C9CEA]"><i
                 class="fa-brands fa-twitter"></i></a></button>
           <button class="w-[2.5rem] h-[2.5rem] rounded-lg bg-[#212425] ml-[15px]"> <a
               href="https://github.com/naresh515?fbclid=PAAaZUwv63b3s-1QU2oKR5eoYGZJzcNUHih6vupC6vAN5lON1OK3Lb_eG75p8"
-              class="text-[#e14a84]"><i class="fa-brands fa-github"></i></a></button>
+              target="_blank" class="text-[#e14a84]"><i class="fa-brands fa-github"></i></a></button>
           <button class="w-[2.5rem] h-[2.5rem] rounded-lg bg-[#212425] ml-[15px]"><a
               href="https://www.linkedin.com/in/naresh-tak-90b9a9209?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app"
-              class="text-[#0072b1]"><i class="fa-brands fa-linkedin"></i></a></button>
+              target="_blank" class="text-[#0072b1]"><i class="fa-brands fa-linkedin"></i></a></button>
         </div>
         <div class="contact_info bg-[#1D1D1D] p-2 lg:p-7 rounded-2xl my-7">
           <div class="flex py-2.5 border-b border-[#3D3A3A] gap-2 lg:gap-3.5">
@@ -120,7 +121,7 @@ const showTopContainer = computed(() => {
                 class="fa-solid fa-location-dot text-[#6AB5B8]"></i></button>
             <div class="flex flex-col items-baseline">
               <span class="text-xs text-white">Location</span>
-              <span><a href="tel:8279214452" class="text-white">Bikaner, Rajasthan, India</a></span>
+              <span><a class="text-white">Bikaner, Rajasthan, India</a></span>
             </div>
           </div>
           <div class="flex py-2.5 border-b border-[#3D3A3A] gap-2 lg:gap-3.5">
@@ -128,7 +129,8 @@ const showTopContainer = computed(() => {
                 class="fa-solid fa-envelope text-[#FD7590]"></i></button>
             <div class="flex flex-col items-baseline">
               <span class="text-xs text-white">Email</span>
-              <span><a href="mailto:taknaresh515@gmail.com" class="text-white hover:text-[#FA5252]">taknaresh515@gmail.com</a></span>
+              <span><a href="mailto:taknaresh515@gmail.com"
+                  class="text-white hover:text-[#FA5252]">taknaresh515@gmail.com</a></span>
             </div>
           </div>
           <div class="flex py-2.5 gap-2 lg:gap-3.5">
@@ -136,7 +138,7 @@ const showTopContainer = computed(() => {
                 class="fa-solid fa-calendar-days text-[#C17CEB]"></i></button>
             <div class="flex flex-col items-baseline">
               <span class="text-xs text-white">Birthday</span>
-              <span><a href="tel:8279214452" class="text-white">April 15, 1997</a></span>
+              <span><a class="text-white">April 15, 1997</a></span>
             </div>
           </div>
         </div>
