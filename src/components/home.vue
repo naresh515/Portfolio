0d78182bdfb1e16d9852b35af8e808d9ffbc8007
Naresh Tak <taknaresh515@gmail.com>
2024-02-13 12:47:03 +0530

    under text

--- src/components/home.vue
+++ src/components/home.vue
@@ -65,6 +65,24 @@
                 </div>
             </div>
         </div>
+        <div class="py-[10px] text-sm mt-8 rounded-[0.75rem] bg-[#212425]">
+            <p class="text-white text-center flex items-center justify-center gap-4">This website created on
+                <a href="https://vuejs.org/" target="_blank"><svg class="logo" viewBox="0 0 128 128" width="24" height="24"
+                        data-v-e9cb7cd5="">
+                        <path fill="#42b883" d="M78.8,10L64,35.4L49.2,10H0l64,110l64-110C128,10,78.8,10,78.8,10z"
+                            data-v-e9cb7cd5=""></path>
+                        <path fill="#35495e" d="M78.8,10L64,35.4L49.2,10H25.6L64,76l38.4-66H78.8z" data-v-e9cb7cd5="">
+                        </path>
+                    </svg></a> &
+                <a href="https://tailwindcss.com/" target="_blank"><svg viewBox="0 0 248 31"
+                        class="text-slate-900 dark:text-white w-auto h-5">
+                        <path fill-rule="evenodd" clip-rule="evenodd"
+                            d="M25.517 0C18.712 0 14.46 3.382 12.758 10.146c2.552-3.382 5.529-4.65 8.931-3.805 1.941.482 3.329 1.882 4.864 3.432 2.502 2.524 5.398 5.445 11.722 5.445 6.804 0 11.057-3.382 12.758-10.145-2.551 3.382-5.528 4.65-8.93 3.804-1.942-.482-3.33-1.882-4.865-3.431C34.736 2.92 31.841 0 25.517 0zM12.758 15.218C5.954 15.218 1.701 18.6 0 25.364c2.552-3.382 5.529-4.65 8.93-3.805 1.942.482 3.33 1.882 4.865 3.432 2.502 2.524 5.397 5.445 11.722 5.445 6.804 0 11.057-3.381 12.758-10.145-2.552 3.382-5.529 4.65-8.931 3.805-1.941-.483-3.329-1.883-4.864-3.432-2.502-2.524-5.398-5.446-11.722-5.446z"
+                            fill="#38bdf8"></path>
+                    </svg>
+                </a>
+            </p>
+        </div>
     </div>
 </template>
 
@@ -86,4 +104,5 @@
         left: 7rem;
         width: 10rem;
     }
-}</style>
+}
+</style>
