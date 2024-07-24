96608679d5ec02289bb9a36fbcdd13fe8d5cca4e
Naresh Tak <taknaresh515@gmail.com>
2024-07-24 10:25:57 +0530

    add technology

--- src/components/Resume.vue
+++ src/components/Resume.vue
@@ -91,42 +91,42 @@
                     <div class="mb-7">
                         <div class="flex justify-between py-1">
                             <span class="text-base text-gray-lite font-semibold text-[#A6A6A6]">Tailwind</span>
-                            <span class="text-base font-semibold text-gray-lite pr-5 text-[#A6A6A6]">90%</span>
+                            <span class="text-base font-semibold text-gray-lite pr-5 text-[#A6A6A6]">100%</span>
                         </div>
                         <svg class="rc-progress-line" viewBox="0 0 100 1" preserveAspectRatio="none">
                             <path class="rc-progress-line-trail" d="M 0.5,0.5
          L 99.5,0.5" stroke-linecap="round" stroke="#D9D9D9" stroke-width="1" fill-opacity="0"></path>
                             <path class="rc-progress-line-path" d="M 0.5,0.5
          L 99.5,0.5" stroke-linecap="round" stroke="#5185D4" stroke-width="1" fill-opacity="0"
-                                style="stroke-dasharray: 90px, 100px; stroke-dashoffset: 0px; transition: stroke-dashoffset 0.3s ease 0s, stroke-dasharray 0.3s ease 0s, stroke 0.3s linear 0s, 0.06s;">
+                                style="stroke-dasharray: 100px, 100px; stroke-dashoffset: 0px; transition: stroke-dashoffset 0.3s ease 0s, stroke-dasharray 0.3s ease 0s, stroke 0.3s linear 0s, 0.06s;">
                             </path>
                         </svg>
                     </div>
                     <div class="mb-7">
                         <div class="flex justify-between py-1">
                             <span class="text-base text-gray-lite font-semibold text-[#A6A6A6]">BootStrap</span>
-                            <span class="text-base font-semibold text-gray-lite pr-5 text-[#A6A6A6]">70%</span>
+                            <span class="text-base font-semibold text-gray-lite pr-5 text-[#A6A6A6]">90%</span>
                         </div>
                         <svg class="rc-progress-line" viewBox="0 0 100 1" preserveAspectRatio="none">
                             <path class="rc-progress-line-trail" d="M 0.5,0.5
          L 99.5,0.5" stroke-linecap="round" stroke="#D9D9D9" stroke-width="1" fill-opacity="0"></path>
                             <path class="rc-progress-line-path" d="M 0.5,0.5
          L 99.5,0.5" stroke-linecap="round" stroke="#CA56F2" stroke-width="1" fill-opacity="0"
-                                style="stroke-dasharray: 70px, 100px; stroke-dashoffset: 0px; transition: stroke-dashoffset 0.3s ease 0s, stroke-dasharray 0.3s ease 0s, stroke 0.3s linear 0s, 0.06s;">
+                                style="stroke-dasharray: 90px, 100px; stroke-dashoffset: 0px; transition: stroke-dashoffset 0.3s ease 0s, stroke-dasharray 0.3s ease 0s, stroke 0.3s linear 0s, 0.06s;">
                             </path>
                         </svg>
                     </div>
                     <div class="mb-7">
                         <div class="flex justify-between py-1">
                             <span class="text-base text-gray-lite font-semibold text-[#A6A6A6]">Github</span>
-                            <span class="text-base font-semibold text-gray-lite pr-5 text-[#A6A6A6]">80%</span>
+                            <span class="text-base font-semibold text-gray-lite pr-5 text-[#A6A6A6]">100%</span>
                         </div>
                         <svg class="rc-progress-line" viewBox="0 0 100 1" preserveAspectRatio="none">
                             <path class="rc-progress-line-trail" d="M 0.5,0.5
          L 99.5,0.5" stroke-linecap="round" stroke="#D9D9D9" stroke-width="1" fill-opacity="0"></path>
                             <path class="rc-progress-line-path" d="M 0.5,0.5
          L 99.5,0.5" stroke-linecap="round" stroke="#4c0519" stroke-width="1" fill-opacity="0"
-                                style="stroke-dasharray: 80px, 100px; stroke-dashoffset: 0px; transition: stroke-dashoffset 0.3s ease 0s, stroke-dasharray 0.3s ease 0s, stroke 0.3s linear 0s, 0.06s;">
+                                style="stroke-dasharray: 100px, 100px; stroke-dashoffset: 0px; transition: stroke-dashoffset 0.3s ease 0s, stroke-dasharray 0.3s ease 0s, stroke 0.3s linear 0s, 0.06s;">
                             </path>
                         </svg>
                     </div>
@@ -148,14 +148,14 @@
                     <div class="mb-7">
                         <div class="flex justify-between py-1">
                             <span class="text-base text-gray-lite font-semibold text-[#A6A6A6]">React JS</span>
-                            <span class="text-base font-semibold text-gray-lite pr-5 text-[#A6A6A6]">40%</span>
+                            <span class="text-base font-semibold text-gray-lite pr-5 text-[#A6A6A6]">50%</span>
                         </div>
                         <svg class="rc-progress-line" viewBox="0 0 100 1" preserveAspectRatio="none">
                             <path class="rc-progress-line-trail" d="M 0.5,0.5
          L 99.5,0.5" stroke-linecap="round" stroke="#D9D9D9" stroke-width="1" fill-opacity="0"></path>
                             <path class="rc-progress-line-path" d="M 0.5,0.5
          L 99.5,0.5" stroke-linecap="round" stroke="#03adfc" stroke-width="1" fill-opacity="0"
-                                style="stroke-dasharray: 40px, 100px; stroke-dashoffset: 0px; transition: stroke-dashoffset 0.3s ease 0s, stroke-dasharray 0.3s ease 0s, stroke 0.3s linear 0s, 0.06s;">
+                                style="stroke-dasharray: 50px, 100px; stroke-dashoffset: 0px; transition: stroke-dashoffset 0.3s ease 0s, stroke-dasharray 0.3s ease 0s, stroke 0.3s linear 0s, 0.06s;">
                             </path>
                         </svg>
                     </div>
@@ -164,28 +164,28 @@
                     <div class="mb-7">
                         <div class="flex justify-between py-1">
                             <span class="text-base text-gray-lite font-semibold text-[#A6A6A6]">JavaScript</span>
-                            <span class="text-base font-semibold text-gray-lite pr-5 text-[#A6A6A6]">60%</span>
-                        </div>
-                        <svg class="rc-progress-line" viewBox="0 0 100 1" preserveAspectRatio="none">
-                            <path class="rc-progress-line-trail" d="M 0.5,0.5
-         L 99.5,0.5" stroke-linecap="round" stroke="#D9D9D9" stroke-width="1" fill-opacity="0"></path>
-                            <path class="rc-progress-line-path" d="M 0.5,0.5
-         L 99.5,0.5" stroke-linecap="round" stroke="#0d9488" stroke-width="1" fill-opacity="0"
-                                style="stroke-dasharray: 60px, 100px; stroke-dashoffset: 0px; transition: stroke-dashoffset 0.3s ease 0s, stroke-dasharray 0.3s ease 0s, stroke 0.3s linear 0s, 0.06s;">
-                            </path>
-                        </svg>
-                    </div>
-                    <div class="mb-7">
-                        <div class="flex justify-between py-1">
-                            <span class="text-base text-gray-lite font-semibold text-[#A6A6A6]">Jquery</span>
                             <span class="text-base font-semibold text-gray-lite pr-5 text-[#A6A6A6]">70%</span>
                         </div>
                         <svg class="rc-progress-line" viewBox="0 0 100 1" preserveAspectRatio="none">
                             <path class="rc-progress-line-trail" d="M 0.5,0.5
          L 99.5,0.5" stroke-linecap="round" stroke="#D9D9D9" stroke-width="1" fill-opacity="0"></path>
                             <path class="rc-progress-line-path" d="M 0.5,0.5
-         L 99.5,0.5" stroke-linecap="round" stroke="#9333ea" stroke-width="1" fill-opacity="0"
+         L 99.5,0.5" stroke-linecap="round" stroke="#0d9488" stroke-width="1" fill-opacity="0"
                                 style="stroke-dasharray: 70px, 100px; stroke-dashoffset: 0px; transition: stroke-dashoffset 0.3s ease 0s, stroke-dasharray 0.3s ease 0s, stroke 0.3s linear 0s, 0.06s;">
+                            </path>
+                        </svg>
+                    </div>
+                    <div class="mb-7">
+                        <div class="flex justify-between py-1">
+                            <span class="text-base text-gray-lite font-semibold text-[#A6A6A6]">Jquery</span>
+                            <span class="text-base font-semibold text-gray-lite pr-5 text-[#A6A6A6]">90%</span>
+                        </div>
+                        <svg class="rc-progress-line" viewBox="0 0 100 1" preserveAspectRatio="none">
+                            <path class="rc-progress-line-trail" d="M 0.5,0.5
+         L 99.5,0.5" stroke-linecap="round" stroke="#D9D9D9" stroke-width="1" fill-opacity="0"></path>
+                            <path class="rc-progress-line-path" d="M 0.5,0.5
+         L 99.5,0.5" stroke-linecap="round" stroke="#9333ea" stroke-width="1" fill-opacity="0"
+                                style="stroke-dasharray: 90px, 100px; stroke-dashoffset: 0px; transition: stroke-dashoffset 0.3s ease 0s, stroke-dasharray 0.3s ease 0s, stroke 0.3s linear 0s, 0.06s;">
                             </path>
                         </svg>
                     </div>
@@ -206,14 +206,14 @@
                     <div class="mb-7">
                         <div class="flex justify-between py-1">
                             <span class="text-base text-gray-lite font-semibold text-[#A6A6A6]">SASS / Less</span>
-                            <span class="text-base font-semibold text-gray-lite pr-5 text-[#A6A6A6]">50%</span>
+                            <span class="text-base font-semibold text-gray-lite pr-5 text-[#A6A6A6]">70%</span>
                         </div>
                         <svg class="rc-progress-line" viewBox="0 0 100 1" preserveAspectRatio="none">
                             <path class="rc-progress-line-trail" d="M 0.5,0.5
          L 99.5,0.5" stroke-linecap="round" stroke="#D9D9D9" stroke-width="1" fill-opacity="0"></path>
                             <path class="rc-progress-line-path" d="M 0.5,0.5
          L 99.5,0.5" stroke-linecap="round" stroke="#ea580c" stroke-width="1" fill-opacity="0"
-                                style="stroke-dasharray: 50px, 100px; stroke-dashoffset: 0px; transition: stroke-dashoffset 0.3s ease 0s, stroke-dasharray 0.3s ease 0s, stroke 0.3s linear 0s, 0.06s;">
+                                style="stroke-dasharray: 70px, 100px; stroke-dashoffset: 0px; transition: stroke-dashoffset 0.3s ease 0s, stroke-dasharray 0.3s ease 0s, stroke 0.3s linear 0s, 0.06s;">
                             </path>
                         </svg>
                     </div>
@@ -234,14 +234,14 @@
                     <div class="mb-7">
                         <div class="flex justify-between py-1">
                             <span class="text-base text-gray-lite font-semibold text-[#A6A6A6]">Express JS</span>
-                            <span class="text-base font-semibold text-gray-lite pr-5 text-[#A6A6A6]">50%</span>
+                            <span class="text-base font-semibold text-gray-lite pr-5 text-[#A6A6A6]">70%</span>
                         </div>
                         <svg class="rc-progress-line" viewBox="0 0 100 1" preserveAspectRatio="none">
                             <path class="rc-progress-line-trail" d="M 0.5,0.5
          L 99.5,0.5" stroke-linecap="round" stroke="#D9D9D9" stroke-width="1" fill-opacity="0"></path>
                             <path class="rc-progress-line-path" d="M 0.5,0.5
          L 99.5,0.5" stroke-linecap="round" stroke="#660066" stroke-width="1" fill-opacity="0"
-                                style="stroke-dasharray: 50px, 100px; stroke-dashoffset: 0px; transition: stroke-dashoffset 0.3s ease 0s, stroke-dasharray 0.3s ease 0s, stroke 0.3s linear 0s, 0.06s;">
+                                style="stroke-dasharray: 70px, 100px; stroke-dashoffset: 0px; transition: stroke-dashoffset 0.3s ease 0s, stroke-dasharray 0.3s ease 0s, stroke 0.3s linear 0s, 0.06s;">
                             </path>
                         </svg>
                     </div>
